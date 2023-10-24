896fef06ab07eb7ec734742f30ca2945d027564b
Mehmet Salih Öğün <111509452+ogunmehmetsalih@users.noreply.github.com>
2023-10-24 14:44:57 +0300

    Add files via upload

---
+++ bsg_encryption.py
@@ -0,0 +1,32 @@
+import random
+
+
+def encryption(text):
+    char_number = []
+    char_value = []
+    for char in text:
+        char_number.append(ord(char))
+    for char in char_number:
+        value = ''
+        for i in range(char):
+            value += str(random.randint(0,1))
+        char_value.append(value)
+    return '0'.join(char_value)
+
+
+def decryption(text):
+    char_value = text.split('o')
+    result = ''
+    for i in char_value:
+        try:
+            result += chr(int(len(i)))
+        except:
+            result += i
+    return result
+
+
+text = 'hello world'
+print("şifrelenmiş metin",encryption(text))
+print("çözülmüş metin:",decryption(encryption(text)))
+
+
